fadd5211f9e209da8ce8b1102f7890cd9a385da5
Peeyush_ <peeyush.u@exploride.com>
2017-08-26 03:20:35 +0530

    added listHellobm endpoint

--- app.js
+++ app.js
@@ -20,10 +20,69 @@ app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+var querystring = require('querystring');
+var https = require('https');
+var host = 'api.us.apiconnect.ibmcloud.com';
+app.get('/listHellobm', function (req, res) {
+	
 
+    console.log('rec request 1');
+	var endpoint = '/myown1org-dev/sb/testapihelloworld/getAllHello';
+	var data = '';
+	var method = 'GET';
+	console.log('rec request 2');
+	   performRequest(endpoint, method, data, function (err, data) {
+		   console.log('rec request end');
+	       console.log( data );
+	       res.end( data );
+	   });
+	});
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
 
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+function performRequest(endpoint, method, data, success) {
+	console.log('rec request 3');
+  var dataString = JSON.stringify(data);
+  var headers = {};
+  
+  if (method == 'GET') {
+    endpoint += '?' + querystring.stringify(data);
+  }
+  else {
+    headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': dataString.length
+    };
+  }
+  var options = {
+    host: host,
+    path: endpoint,
+    method: method,
+    headers: headers
+  };
+  console.log('rec request 4');
+  var req = https.request(options, function(res) {
+    res.setEncoding('utf-8');
+    console.log('going to send request');
+
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+    });
+
+    res.on('end', function() {
+      console.log(responseString);
+//      var responseObject = JSON.parse(responseString);
+//      console.log('resp jObj'+responseObject.total_rows);
+      success('', responseString);
+    });
+  });
+
+  req.write(dataString);
+  req.end();
+}
